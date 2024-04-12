bfdeaf2b96ba219e2f22e82988a9c91fe4aa6972
Shaimi Anass <146459468+Anosse-7@users.noreply.github.com>
2024-04-12 18:23:47 +0000

    first commit

---
+++ src/main/java/com/example/gestionticket/GestionTicketApplication.java
@@ -0,0 +1,12 @@
+package com.example.gestionticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionTicketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionTicketApplication.class, args);
+    }
+}
