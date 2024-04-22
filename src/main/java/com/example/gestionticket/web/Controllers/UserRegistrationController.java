d8974607827240d5bc69d39a11d5b6b2090e9d6f
Shaimi Anass <146459468+Anosse-7@users.noreply.github.com>
2024-04-22 23:31:49 +0100

    index page commit

---
+++ src/main/java/com/example/gestionticket/web/Controllers/UserRegistrationController.java
@@ -0,0 +1,67 @@
+package com.example.gestionticket.web.Controllers;
+
+import com.example.gestionticket.Entities.User;
+import com.example.gestionticket.services.UserService;
+import com.example.gestionticket.web.dto.UserRegistrationDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/registration")
+public class UserRegistrationController {
+
+    private final UserService userService;
+    private final Logger logger = LoggerFactory.getLogger(UserRegistrationController.class);
+    private static final Logger log = LoggerFactory.getLogger(UserRegistrationController.class);
+
+    @Autowired
+    public UserRegistrationController(UserService userService){
+        this.userService = userService;
+    }
+
+    @ModelAttribute("user")
+    public UserRegistrationDto userRegistrationDto() {
+        return new UserRegistrationDto();
+    }
+
+    @GetMapping
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new UserRegistrationDto());
+        return "Registration/registration";
+    }
+
+    @PostMapping
+    public String registerUserAccount(@ModelAttribute("user") @Valid UserRegistrationDto registrationDto, BindingResult result) {
+        User existingUser = userService.findByUsername(registrationDto.getUsername());
+        if (existingUser != null) {
+            result.rejectValue("username", null, "There is already an account registered with that username");
+        }
+
+        if (!registrationDto.getPassword().equals(registrationDto.getRepeatPassword())) {
+            result.rejectValue("repeatPassword", null, "Passwords don't match");
+        }
+
+        if (result.hasErrors()) {
+            return "Registration/registration";
+        }
+
+        User user = registrationDto.toUser();
+        User savedUser = userService.save(user);
+        if (savedUser == null) {
+            logger.error("User could not be saved");
+            result.rejectValue("username", null, "User could not be saved");
+            return "Registration/registration";
+        }
+        return "redirect:/registration?success";
+    }
+}
---
+++ src/main/java/com/example/gestionticket/web/dto/UserRegistrationDto.java
@@ -0,0 +1,50 @@
+package com.example.gestionticket.web.dto;
+
+import com.example.gestionticket.Entities.User;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Setter
+@Getter
+public class UserRegistrationDto {
+
+    private String username;
+    private String nom;
+    private String prenom;
+    private String email;
+    private String password;
+    private String repeatPassword;
+    private String addresse;
+    private String telephone;
+    private boolean active;
+    private String role;
+
+    public UserRegistrationDto() {
+
+    }
+    // Add this method to the UserRegistrationDto class
+public User toUser() {
+    User user = new User();
+    user.setUsername(this.username);
+    user.setNom(this.nom);
+    user.setPrenom(this.prenom);
+    user.setEmail(this.email);
+    user.setPassword(this.password);
+    user.setAddresse(this.addresse);
+    user.setTelephone(this.telephone);
+    user.setActive(this.active);
+    if (this.role == null || this.role.isEmpty()) {
+        user.setRole("Client"); // replace "DEFAULT_ROLE" with your default role
+    } else {
+        user.setRole(this.role);
+    }
+    return user;
+}
+
+    public String getRepeatPassword() {
+        return password;
+    }
+}
+
