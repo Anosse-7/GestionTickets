0039fd08160d735538770950c2c84cc9abb54179
Shaimi Anass <146459468+anosse-7@users.noreply.github.com>
2024-05-05 20:11:17 +0100

    crud Evenement 3

--- src/main/java/com/example/gestionticket/services/EvenementService.java
+++ src/main/java/com/example/gestionticket/services/EvenementService.java
@@ -1,17 +1,15 @@
 package com.example.gestionticket.services;
 
 import com.example.gestionticket.Entities.Evenement;
-import com.example.gestionticket.Repository.EvenementRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class EvenementService {
-    @Autowired private EvenementRepository event;
+public interface EvenementService {
 
-    public List<Evenement> listAll(){
-return (List<Evenement>) event.findAll();
-    }
+    List<Evenement> listAll();
+    Evenement getCurrentEvent(Long id);
+    Evenement updateEvent(Evenement evenement);
+    Evenement addEvent(Evenement evenement);
 }
