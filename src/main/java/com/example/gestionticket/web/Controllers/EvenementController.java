6e1c9a4788a725cc1e0c6c843ffeffc14b08530b
Shaimi Anass <146459468+Anosse-7@users.noreply.github.com>
2024-05-05 18:49:07 +0100

    first anass commit

--- src/main/java/com/example/gestionticket/web/Controllers/EvenementController.java
+++ src/main/java/com/example/gestionticket/web/Controllers/EvenementController.java
@@ -1,6 +1,5 @@
 package com.example.gestionticket.web.Controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +9,7 @@ import java.security.PrivateKey;
 @Controller
 @RequestMapping("/Evenement")
 public class EvenementController {
+
     @GetMapping
     public String Event(){
         System.out.println("Evenement Controller");
